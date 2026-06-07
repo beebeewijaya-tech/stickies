64942161d39ce59a524ba9fc37cddb5ce71960f9
Bee Wijaya <beebeewijaya.tech@gmail.com>
2026-06-07 19:49:23 +0700

    Initial Commit

---
+++ Stickies/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stickies
+//
+//  Created by Bee Wijaya on 07/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stickies/StickiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickiesApp.swift
+//  Stickies
+//
+//  Created by Bee Wijaya on 07/06/26.
+//
+
+import SwiftUI
+
+@main
+struct StickiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
